af34a216b54074bc76841e86d64bd95deff78fa9
Benjamin Congdon <bencongdon96@gmail.com>
2017-02-09 19:05:06 -0600

    Rollback structure

--- Lab1/utils.py
+++
@@ -1,7 +0,0 @@
-import re
-import string
-
-
-def strip_punctuation(str_in):
-    # Strip punctuation from word (don't worry too much about this)
-    return re.sub('[%s]' % re.escape(string.punctuation), '', str_in)
